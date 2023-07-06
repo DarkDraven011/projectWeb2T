4c11ac74d5b859604e95df3dd8fb18e03ae29114
darkraven011 <137371369+darkraven011@users.noreply.github.com>
2023-07-06 20:49:11 +0700

    5th commit

---
+++ Web2T/Web2T/ModelViews/ProductHomeVM.cs
@@ -0,0 +1,10 @@
+﻿using Web2T.Models;
+
+namespace Web2T.ModelViews
+{
+    public class ProductHomeVM
+    {
+        public Category category { get; set; }
+        public List<Product> lsProducts { get; set; }
+    }
+}
---
+++ Web2T/Web2T/Models/Shipper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Web2T.Models;
+
+public partial class Shipper
+{
+    public int ShipperId { get; set; }
+
+    public string? ShipperName { get; set; }
+
+    public string? Phone { get; set; }
+
+    public string? Company { get; set; }
+
+    public string? ShipDate { get; set; }
+}
