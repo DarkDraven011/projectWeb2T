240c33275472cb7aeb4f78eb27970420976b339f
darkraven011 <137371369+darkraven011@users.noreply.github.com>
2023-07-20 21:11:50 +0700

    7th commit

---
+++ Web2T/Web2T/ModelViews/HomeViewVM.cs
@@ -0,0 +1,10 @@
+﻿using Web2T.Models;
+
+namespace Web2T.ModelViews
+{
+    public class HomeViewVM
+    {
+        public List<Post> Posts { get; set; }
+        public List<ProductHomeVM> Products { get; set; }
+    }
+}
