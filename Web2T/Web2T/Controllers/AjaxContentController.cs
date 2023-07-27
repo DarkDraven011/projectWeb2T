a63d9f3a5044b4b959c70e7448a8d8de5f271a69
darkraven011 <137371369+darkraven011@users.noreply.github.com>
2023-07-27 20:49:47 +0700

    9th commit

---
+++ Web2T/Web2T/Controllers/AjaxContentController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web2T.Controllers
+{
+    public class AjaxContentController : Controller
+    {
+        public IActionResult HeaderCart()
+        {
+            return ViewComponent("HeaderCart");
+        }
+        public IActionResult HeaderFavourites()
+        {
+            return ViewComponent("NumberCart");
+        }
+    }
+}
---
+++ Web2T/Web2T/ModelViews/ChangeAddressViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Web2T.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Web2T.ModelViews
+{
+    public class ChangeAddressViewModel
+    {
+        [Key]
+        public int CustomerId { get; set; }
+
+        [Display(Name = "Địa chỉ hiện tại")]
+        public string AddressNow { get; set; }
+
+        [Display(Name = "Địa chỉ mới")]
+        [Required(ErrorMessage = "Vui lòng nhập địa chỉ mới")]
+        [MinLength(5, ErrorMessage = "Bạn cần thêm địa chỉ mới tối thiểu 5 ký tự")]
+        public string Address { get; set; }
+
+    }
+}
