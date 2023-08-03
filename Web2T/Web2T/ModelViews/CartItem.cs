0e625ea81b628bff4d9a36c6e66c2a5eaa16dc58
darkraven011 <137371369+darkraven011@users.noreply.github.com>
2023-08-03 20:43:01 +0700

    11th commit

---
+++ Web2T/Web2T/ModelViews/CartItem.cs
@@ -0,0 +1,12 @@
+﻿using Web2T.Models;
+
+namespace Web2T.ModelViews
+{
+    public class CartItem
+    {
+        public Product product { get; set; }
+        public int amount { get; set; }
+        public double discountPrice => (product.Price - ((product.Discount * 0.01) * product.Price)).Value;
+        public double TotalMoney => amount * (product.Price - ((product.Discount * 0.01) * product.Price)).Value;
+    }
+}
---
+++ Web2T/Web2T/Models/Account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Web2T.Models;
+
+public partial class Account
+{
+    public int AccountId { get; set; }
+
+    public string Phone { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public string Salt { get; set; } = null!;
+
+    public bool Active { get; set; }
+
+    public string FullName { get; set; } = null!;
+
+    public int? RoleId { get; set; }
+
+    public DateTime? LastLogin { get; set; }
+
+    public DateTime? CreateDate { get; set; }
+
+    public virtual Role? Role { get; set; }
+}
